ba110eedddafa536ddf41fc212ca8e79c2c5bd87
Gabriel <gbevi281@gmail.com>
2026-06-20 23:55:02 -0300

    move useLoop into hooks/ alongside useScramble

--- src/components/cycling-word.tsx
+++ src/components/cycling-word.tsx
@@ -1,22 +1,8 @@
 "use client";
 
 import { AnimatePresence, motion } from "motion/react";
-import { useCallback, useEffect, useState } from "react";
 
-export const useLoop = (delay = 1000) => {
-  const [key, setKey] = useState(0);
-
-  const incrementKey = useCallback(() => {
-    setKey((prev) => prev + 1);
-  }, []);
-
-  useEffect(() => {
-    const interval = setInterval(incrementKey, delay);
-    return () => clearInterval(interval);
-  }, [delay, incrementKey]);
-
-  return { key };
-};
+import { useLoop } from "@/hooks/use-loop";
 
 type Props = {
   words: string[];
